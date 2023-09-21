be06eaccd39f333a87ce19d0a045e29bfc90daca
ATalemsi <136909197+ATalemsi@users.noreply.github.com>
2023-09-21 17:31:54 +0100

    Add files via upload

--- Projet_SAS/main.c
+++ Projet_SAS/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include<time.h>
 typedef struct date{
     int annee;
     int mois;
@@ -41,7 +42,7 @@ void ajouter_nouveaux_tache(tache *taches){
     printf(" entrer le annee de deadline de la tache : ");
     scanf("%d",&taches[tai].deadline.annee);
     taches[tai].statut=(char*)malloc(sizeof(char*));
-    printf("entrer le statut  de la tache : ecrit a realiser ||  en cours de realisation || finalisee : ");
+    printf("entrer le statut  de la tache : ecrit TO DO ||  DOING  || DONE : ");
     scanf(" %[^\n]", taches[tai].statut);
     tai++;
 }
@@ -54,25 +55,19 @@ void ajouter_nouveaux_tache(tache *taches){
 
         }
 
-void tri_alpha(tache taches[]){
-    int i;
-    int j;
-    tache tmp;
-    int min;
-     if(tai==0){
-        printf("la table vide");
-        return;
-     }else{
-       for(i=0;i<=tai-2;i++){
-            min=i;
-       for(j=0;j<tai-i-1;j++)
-            taches[j].titre[0]=toupper(taches[j].titre[0]);
-            if(strcmp(taches[j].titre,taches[j+1].titre)>0){
-                tmp=taches[i];
-                taches[i]=taches[j];
-                taches[j]=tmp;
-            }
-     }
+void tri_alphabetique(tache taches[]){
+    tache temp;
+    for (int i = 0; i<tai; i++)
+    {
+        for (int j = i+1; j < tai; j++)
+        {
+             if (strcmp(taches[i].titre,taches[j].titre)>0)
+        {
+            temp=taches[i];
+            taches[i]=taches[j];
+            taches[j]=temp;
+        }
+        }
     }
 }
 int  comp_date(date d1, date d2){
@@ -98,28 +93,41 @@ void tri_date(tache taches[]){
                taches[j+1]=tmp;
             }
         }
-
      }
 }
-/*
-void  comp_date2(date d1, date d2){
-   if(d1.annee!=d2.annee){
-    return d1.annee - d2.annee;
-   }
-    if(d1.mois!=d2.mois){
-    return d1.mois - d2.mois;
+
+void dead_line(tache taches[]){
+int i;
+int delai_jour;
+int jours;
+    int tab[tai];
+    time_t seconds=time(NULL);
+    struct tm *current_time=localtime(&seconds);
+    int currentYear = (current_time->tm_year + 1900);
+    int currentMonth =(current_time->tm_mon + 1);
+    int currentDay =current_time->tm_mday;
+
+    for (i = 0; i < tai; i++){
+        jours = taches[i].deadline.annee*365+taches[i].deadline.mois*30+taches[i].deadline.jour;
+        delai_jour = jours-(currentYear*365+currentMonth*30+currentDay);
+        tab[i]= delai_jour;
     }
-    if(d1.jour!=d2.jour){
-    return d1.jour - d2.jour;
-   }
+    for (int i = 0; i < tai; i++)
+    {
+        if(tab[i]==0){
+           printf("id : %d | Titre : %s | Deadline en : Aujourdui\n",taches[i].id,taches[i].titre);
+        }else if (tab[i]<=3)
+        {
+           printf("id : %d | Titre : %s | Deadline en : %d jours\n",taches[i].id,taches[i].titre,tab[i]);
+        }
+    }
 }
-*/
 void modifier_desc(tache taches[], int identi){
     int i;
     int t=0;
     for(i=0;i<tai;i++){
         if(taches[i].id==identi){
-                printf("modifier votre description : ");
+               printf("modifier votre description : ");
                scanf(" %[^\n]",taches[i].desc);
                t=1;
         }
@@ -144,13 +152,13 @@ void modifier_status(tache taches[], int identi){
     }
 
 }
-void modifier_status(tache taches[], int identi){
+void modifier_deadline(tache taches[], int identi){
     int i;
     int t=0;
     for(i=0;i<tai;i++){
         if(taches[i].id==identi){
-                printf("modifier date de deadline : \n");
-                printf("entre le nouveaux jour : ");
+               printf("modifier date de deadline : \n");
+               printf("entre le nouveaux jour : ");
                scanf(" %d",taches[i].deadline.jour);
                printf("entre le nouveaux mois : ");
                scanf(" %d",taches[i].deadline.mois);
@@ -165,19 +173,122 @@ void modifier_status(tache taches[], int identi){
 
 }
 
+void recherche_par_id(tache taches[],int identi){
+     int i;
+      tache ta;
+      int t=0;
+     for(i=0;i<tai;i++){
+        if(taches[i].id==identi){
+        ta=taches[i];
+        printf("------tache %d---- \n",ta.id);
+        printf("titre : %s \n",ta.titre);
+        printf("description : %s \n",ta.desc);
+        printf("statut : %s \n",ta.statut);
+        printf("la date jj/mm/aaaa : %d/%d/%d \n",ta.deadline.jour,ta.deadline.mois,ta.deadline.annee);
+        }
+     }
+    if(t==0){
+      printf("Aucune tache avec identifiant %d na ete trouvee.\n",identi);
+    }
+}
+void recherche_par_titre(tache taches[]){
+      int i;
+      char *tit;
+      tache ta;
+      int t=0;
+      tit = (char*)malloc(sizeof(char*));
+      printf("donner le titre de la tache que tu veux rechercher : ");
+      scanf(" %[^\n]",tit);
+     for(i=0;i<tai;i++){
+        if(strcmp(taches[i].titre,tit)==0){
+             ta=taches[i];
+             printf("------tache %d---- \n",ta.id);
+             printf("titre : %s \n",ta.titre);
+             printf("description : %s \n",ta.desc);
+             printf("statut : %s \n",ta.statut);
+             printf("la date jj/mm/aaaa : %d/%d/%d \n",ta.deadline.jour,ta.deadline.mois,ta.deadline.annee);
+             t=1;
+        }
+     }
+     if(t==0){
+        printf(" aucun id trouve");
+     }
+    }
+
+
+void nombre_total_tache(tache taches[]){
+     int i;
+     int N_tache=0;
+     for(i=0;i<tai;i++){
+        if(taches[i].id!=0){
+            N_tache++;
+        }
+     }
+      if(N_tache==0){
+        printf("On a aucun tache a cree");
+      }else{
+          printf(" le nombre des tache cree est : %d",N_tache);
+      }
+}
+
+void status_tache(tache taches[]){
+     int i;
+     int complet=0;
+     int incomplet=0;
+     char sta1[]="DONE";
+     char sta2[]="DOING";
+     char sta3[]="TO DO";
+     for(i=0;i<tai;i++){
+          if(strcmp(taches[i].statut,sta1)==0){
+            complet++;
+          }
+          if(strcmp(taches[i].statut,sta2)==0){
+            incomplet++;
+          }
+             if(strcmp(taches[i].statut,sta3)==0){
+            incomplet++;
+          }
+     }
+     printf(" les nombres des tache incomplet :  %d",incomplet);
+        printf(" les nombres des tache complet :  %d",complet);
+
+}
+
+void jours_restant(tache taches[]){
+int i;
+int delai_jour;
+int jours;
+    int tab[tai];
+    time_t seconds=time(NULL);
+    struct tm *current_time=localtime(&seconds);
+    int currentYear = (current_time->tm_year + 1900);
+    int currentMonth =(current_time->tm_mon + 1);
+    int currentDay =current_time->tm_mday;
+
+    for (i = 0; i < tai; i++){
+        jours = taches[i].deadline.annee*365+taches[i].deadline.mois*30+taches[i].deadline.jour;
+        delai_jour = jours-(currentYear*365+currentMonth*30+currentDay);
+        tab[i]= delai_jour;
+    }
+    for (int i = 0; i < tai; i++)
+    {
+    printf("id : %d | Titre : %s | le nombre des jour rester : %d \n",taches[i].id,taches[i].titre,tab[i]);
+    }
+}
 void supp_tache(tache taches[],int identi){
     int i ,j;
     int t=0;
     for(i=0;i<tai;i++){
-        if(taches[i].id=identi){
+        if(taches[i].id==identi){
                 t=1;
-            for(j=i;j<identi-1;j++){
+            for(j=i;j<tai-1;j++){
                 taches[j]=taches[j+1];
+
             }
         }
-        tai--;
-        return;
     }
+    tai--;
+
       if(t==0){
       printf("Aucune tache avec identifiant %d na ete trouvee.\n",identi);
     }
@@ -203,21 +314,60 @@ void afficher_tache(tache *taches){
         }
     }
     }
+
+
 int main()
 {
-    tache taches[100];
-    int nbr=2;
-   ajouter_nouveaux_tache(taches);
-   //ajouter_plusieur(taches,nbr);
-   //tri_alpha( taches);
-   // tri_date(taches);
-   afficher_tache(taches);
-   modifier_desc(taches,1);
-   afficher_tache(taches);
+    tache taches[150];
+    int nbr;
+    char choice;
+    int choix,choix1,choix2,choix3,choix4;
+    do{
+       printf("\t -----saisir votre choix----- : ");
+
+       printf("\t1-Ajouter nouveaux tache :\n");
+       printf("\t2-Ajouter plusieur tache : \n");
+       printf("\t3-Affichage : \n");
+       printf("\t4-Supprimer une tache : \n");
+       printf("\t5-Modification : \n");
+       printf("\t6-Rechercher : \n");
+       printf("\t7-Statistique : \n");
+       scanf("%d",&choix);
+       if(choix>0 && choix<8){
+          switch(choix){
+             case 1: ajouter_nouveaux_tache(taches);
+             break;
+             case 2: printf
+
+
+            getchar();
+            printf(" do you want to  continue (y/n)\n") ;
+            scanf("%c",&choice);
+             }
+          }
+
+
+       }while(choice=='Y');
 
 
 
 
 
+    }
+
+
+   //ajouter_nouveaux_tache(taches);
+    ajouter_plusieur(taches,nbr);
+    supp_tache(taches,2);
+    //afficher_tache(taches);
+    //tri_alphabetique(taches);
+    //tri_date(taches);
+    //dead_line(taches);
+    //jours_restant(taches);
+    //recherche_par_id(taches,2);
+    //recherche_par_titre(taches);
+    //afficher_tache(taches);
+   // modifier_desc(taches,1);
+    afficher_tache(taches);
     return 0;
 }
